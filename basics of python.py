790e28fcd9e52052adb48c1b8021de267ef70ff9
Ambika.M <ambikam812@gmail.com>
2025-07-01 08:20:03 +0530

    Added print() notes PDF and related Python code example.

---
+++ basics of python.py
@@ -0,0 +1,8 @@
+print("hello friends") #print syntax (this way )
+print("hai\"python\"") #using escape character (\") 
+      
+print('hai"python"') #or this way
+
+print('hai\'python\'') #or this way
+
+print("""This is 'Python' and it's "awesome"!""")#or this way
