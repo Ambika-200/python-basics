73dcc9f147e774a185156fb58872f86be4cd2d66
Ambika.M <ambikam812@gmail.com>
2025-07-04 07:55:58 +0530

    Python Input Function – Basics
    Today I learned how to take user input in Python and display it.
    ✅ input() is used to take input from the user.
    ✅ The value entered is always of string type by default.

--- basics of python.py
+++ basics of python.py
@@ -1,5 +1,3 @@
-#VARIABLE
-name="Ambika"
-print("My Name is",name)
-print(name,"Learning Python")
-print(name,"Wants to become a python expert")
+#INPUT
+name=input("Enter your name:")
+print("My name is",name)
