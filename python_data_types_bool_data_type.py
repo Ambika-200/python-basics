7929c95ae922c0a6458500ccddb376b451ef6e2c
Ambika.M <ambikam812@gmail.com>
2025-07-05 20:38:04 +0530

    ADDED_python_data_types_bool_data_type

---
+++ python_data_types_bool_data_type.py
@@ -0,0 +1,18 @@
+# Boolean Example
+
+a = True
+b = False
+
+# Logical operations
+print("a AND b:", a and b)
+print("a OR b:", a or b)
+print("NOT a:", not a)
+
+# Comparison gives boolean
+x = 10
+y = 5
+print("Is x > y?", x > y)
+print("Is x == y?", x == y)
+
+
+
