55478cc1d2e81029d0bc32d8fd8f2944bdf907f8
Ambika.M <ambikam812@gmail.com>
2025-07-05 20:39:28 +0530

    ADDED_python_data_types_string_data_type

---
+++ python_data_types_string_data_type.py
@@ -0,0 +1,10 @@
+# String Example
+name = "Ambika"
+
+# String operations
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
+print("First 3 letters:", name[:3])
+print("Length of name:", len(name))
+
+
